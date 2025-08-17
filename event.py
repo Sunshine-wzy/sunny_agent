21240783b7d12aa9a7c79d17dd2a2ea0d700794a
Sunshine-wzy <sunshinewzy@qq.com>
2025-08-17 23:46:47 +0800

    添加概率回复消息

--- event.py
+++ event.py
@@ -1,4 +1,5 @@
 import asyncio
+import random
 from nonebot import on_message
 from nonebot.rule import to_me
 from nonebot.adapters.onebot.v11 import Bot, GroupMessageEvent, PrivateMessageEvent
@@ -32,10 +33,16 @@ async def handle_llm_user(event: PrivateMessageEvent, bot: Bot):
 
 
 @mem.handle()
-async def handle_mem_group(event: GroupMessageEvent):
+async def handle_mem_group(event: GroupMessageEvent, bot: Bot):
     if get_group_mem_enabled(event.group_id):
         user_id = event.sender.user_id
         if user_id:
             mem_user_id = f"u{user_id}"
             asyncio.create_task(add_memory(event.message.__str__(), user_id=mem_user_id))
+            
+            # 概率回复消息
+            if random.random() < 0.1:
+                response = await chat.group_chat(event, bot, True)
+                await mem.finish(response)
+            
     await mem.finish()
